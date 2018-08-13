8a915ed1442e46911284ccf5a85c813e37811c2a
Tyler Dallwig <tsdallw@ip-10-113-146-157.evoforge.org>
2018-08-13 18:36:11 +0000

    first commit

---
+++ src/components/ToDoList.vue
@@ -0,0 +1,212 @@
+<template>
+  <div>
+    <input type="text" class="todo-input" placeholder="What needs to be done?" v-model="newToDo" @keyup.enter="addToDo">
+    <todo-item v-for="(todo, index) in todosFiltered" :key="todo.id" :todo="todo" :index="index" :checkAll="!anyRemaining" @removedTodo="removeToDo" @finishedEdit="finishedEdit">
+    </todo-item>
+
+    <div class="extra-container">
+      <div><label><input type="checkbox" :checked="!anyRemaining"
+      @change="checkAllToDos"> Check All</label></div>
+      <div>{{ remaining }} items left</div>
+    </div>
+
+    <div class="extra-container">
+      <div>
+        <button :class="{ active: filter === 'all' }" @click="filter = 'all'">All</button>
+        <button :class="{ active: filter === 'active' }" @click="filter = 'active'">Active</button>
+        <button :class="{ active: filter === 'completed' }" @click="filter = 'completed'">Completed</button>
+      </div>
+
+      <div>
+        <transition name="fade">
+        <button v-if="showClearCompletedButton" @click="clearCompleted">Clear Completed</button>
+        </transition>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+import ToDoListItem from './ToDoListItem.vue'
+
+export default {
+  name: 'todo-list',
+  components: {
+    'todo-item' : ToDoListItem
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      newToDo: '',
+      idForTodo: 4,
+      beforeEditCache: '',
+      filter: 'all',
+      todos: [
+        {
+          'id': 1,
+          'title': 'Example To Do 1',
+          'completed': false,
+          'editing': false
+        },
+        {
+          'id': 2,
+          'title': 'Example To Do 2',
+          'completed': false,
+          'editing': false
+        },
+        {
+          'id': 3,
+          'title': 'Example To Do 3',
+          'completed': false,
+          'editing': false
+        }
+      ]
+    }
+  },
+  methods: {
+    addToDo () {
+      if (this.newToDo.trim().length === 0) {
+        return
+      }
+      this.todos.push({
+        id: this.idForTodo,
+        title: this.newToDo,
+        completed: false,
+        editing: false
+      })
+
+      this.newToDo = ''
+      this.idForTodo++
+    },
+    removeToDo (index) {
+      this.todos.splice(index, 1)
+    },
+    checkAllToDos () {
+      this.todos.forEach((todo) => (todo.completed = event.target.checked))
+    },
+    clearCompleted () {
+      this.todos = this.todos.filter(todo => !todo.completed)
+    },
+    finishedEdit(data){
+      this.todos.splice(data.index, 1, data.todo)
+    }
+  },
+  computed: {
+    remaining () {
+      return this.todos.filter(todo => !todo.completed).length
+    },
+    anyRemaining () {
+      return this.remaining !== 0
+    },
+    todosFiltered () {
+      if (this.filter === 'all') {
+        return this.todos
+      } else if (this.filter === 'active') {
+        return this.todos.filter(todo => !todo.completed)
+      } else if (this.filter === 'completed') {
+        return this.todos.filter(todo => todo.completed)
+      }
+      return this.todos
+    },
+    showClearCompletedButton () {
+      return this.todos.filter(todo => todo.completed).length > 0
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @import url("https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css");
+
+  .todo-input {
+    width: 100%;
+    padding: 10px 18px;
+    font-size: 18px;
+    margin-bottom: 16px;
+
+    &:focus {
+      outline: 0;
+    }
+  }
+
+  .todo-item{
+    margin-bottom: 12px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    animation-duration: 0.3s;
+  }
+
+  .remove-item {
+    cursor: pointer;
+    margin-left: 14px;
+    &:hover {
+      color: black;
+    }
+  }
+
+  .todo-item-left {
+    display: flex;
+    align-items: center;
+  }
+
+  .todo-item-label {
+    padding: 10px;
+    border: 1px solid white;
+    margin-left: 12px;
+  }
+
+  .todo-item-edit {
+    font-size: 24px;
+    color: #2c3e50;
+    margin-left: 12px;
+    width: 100%;
+    padding: 10px;
+    border: 1px solid #ccc;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  }
+
+  .completed {
+    text-decoration: line-through;
+    color: grey;
+  }
+
+  .extra-container {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 16px;
+    border-top: 1px solid lightgrey;
+    padding-top: 14px;
+    margin-bottom: 14px;
+  }
+
+  button {
+    font-size: 14px;
+    background-color: white;
+
+    &:hover {
+      background: lightgreen;
+    }
+
+    &:focus {
+      outline: none;
+    }
+  }
+
+  .active {
+    background: lightgreen;
+  }
+
+  // CSS Transitions
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .2s;
+  }
+
+  .fade-enter, .fade-leave-to {
+    opacity: 0;
+  }
+
+</style>
